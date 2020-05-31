d20d13bbe1e956e50026dd75f384eac496b1c37c
55utd55 <samsun55umut@gmail.com>
2020-05-31 13:32:12 +0300

    Update class.php

--- class.php
+++ class.php
@@ -13,73 +13,73 @@ class burc{
 	function yengec(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/yengec/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function kova(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/kova/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function koc(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/koc/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function oglak(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/oglak/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function aslan(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/aslan/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function akrep(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/akrep/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function yay(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/yay/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function ikizler(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/ikizler/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function boga(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/boga/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function basak(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/basak/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function balik(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/balik/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 	function terazi(){
 		$a = file_get_contents("http://mahmure.hurriyet.com.tr/astroloji/burclar/terazi/");
 		$b = ara("<p>","</p>",$a);
-		$yorum = $b[1];
+		$yorum = $b[0];
 		return $yorum;
 	}
 }
